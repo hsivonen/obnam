f0b6e08a2bacdfc4ecf06775d919c765d40c9b52
Lars Wirzenius <liw@iki.fi>
2006-09-24 15:33:56 +0300

    Refactor tests.

--- backendTests.py
+++ backendTests.py
@@ -1,16 +1,27 @@
 import ConfigParser
+import shutil
 import tempfile
 import unittest
 
 import backend
 
 
-class InitTests(unittest.TestCase):
+class LocalBackendBase(unittest.TestCase):
+
+    def setUp(self):
+        self.tempdir = tempfile.mkdtemp()
+        self.config = ConfigParser.ConfigParser()
+        self.config.add_section("local-backend")
+        self.config.set("local-backend", "root", self.tempdir)
+
+    def tearDown(self):
+        shutil.rmtree(self.tempdir)
+        del self.tempdir
+        del self.config
+
+
+class InitTests(LocalBackendBase):
 
     def testInit(self):
-        tempdir = tempfile.mkdtemp()
-        config = ConfigParser.ConfigParser()
-        config.add_section("local-backend")
-        config.set("local-backend", "root", tempdir)
-        be = backend.init(config)
-        self.failUnlessEqual(be.local_root, tempdir)
+        be = backend.init(self.config)
+        self.failUnlessEqual(be.local_root, self.tempdir)
