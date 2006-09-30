6bd544028bf7a0da8f6626ad482e8a21150b55a8
Lars Wirzenius <liw@iki.fi>
2006-09-30 12:10:04 +0300

    Use /dev/null instead of Makefile, since the latter keeps changing.

--- wibbrlibTests.py
+++ wibbrlibTests.py
@@ -212,22 +212,22 @@ class GenerationTests(unittest.TestCase):
 class RsyncTests(unittest.TestCase):
 
     def testSignature(self):
-        sig = wibbrlib.rsync.compute_signature("Makefile")
-        os.system("rdiff signature Makefile Makefile.sig.temp")
-        f = file("Makefile.sig.temp")
+        sig = wibbrlib.rsync.compute_signature("/dev/null")
+        os.system("rdiff signature /dev/null devnull.sig.temp")
+        f = file("devnull.sig.temp")
         data = f.read()
         f.close()
         self.failUnlessEqual(sig, data)
-        os.remove("Makefile.sig.temp")
+        os.remove("devnull.sig.temp")
 
     def testEmptyDelta(self):
-        sig = wibbrlib.rsync.compute_signature("Makefile")
-        delta = wibbrlib.rsync.compute_delta(sig, "Makefile")
+        sig = wibbrlib.rsync.compute_signature("/dev/null")
+        delta = wibbrlib.rsync.compute_delta(sig, "/dev/null")
         # The hex string below is what rdiff outputs. I've no idea what
         # the format is, and the empty delta is expressed differently
         # in different situations. Eventually we'll move away from rdiff,
         # and then this should become clearer. --liw, 2006-09-24
-        self.failUnlessEqual(delta, "rs\x026E\x00b\x00")
+        self.failUnlessEqual(delta, "rs\x026\x00")
 
 
 class NormalizedInodeTests(unittest.TestCase):
