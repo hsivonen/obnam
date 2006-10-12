33502d04fd9a45eaea991673f8bddb2574cf18e5
Lars Wirzenius <liw@iki.fi>
2006-10-12 15:12:55 +0300

    Removed debugging output.

--- testrun.py
+++ testrun.py
@@ -7,7 +7,7 @@ for py in [py for py in os.listdir(".") if py.endswith("Tests.py")]:
     suite.addTest(unittest.defaultTestLoader.loadTestsFromName(py[:-3]))
 
 runner = unittest.TextTestRunner()
-print result
+result = runner.run(suite)
 if result.errors:
     sys.exit(1)
 else:
