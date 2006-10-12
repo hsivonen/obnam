dbf477b0ee91d6251d1b0b8ecc9df165779b0fdc
Lars Wirzenius <liw@iki.fi>
2006-10-12 14:55:31 +0300

    Exit process with non-zero if there were any errors.

--- testrun.py
+++ testrun.py
@@ -1,9 +1,15 @@
 import unittest
 import os
+import sys
 
 suite = unittest.TestSuite()
 for py in [py for py in os.listdir(".") if py.endswith("Tests.py")]:
     suite.addTest(unittest.defaultTestLoader.loadTestsFromName(py[:-3]))
 
 runner = unittest.TextTestRunner()
-runner.run(suite)
+result = runner.run(suite)
+if result.errors == 0:
+    sys.exit(0)
+else:
+    sys.exit(1)
+
