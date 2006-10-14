2c760872db37d4f008b2c9e601597e0ee555b5f9
Lars Wirzenius <liw@iki.fi>
2006-10-14 13:15:34 +0300

    When creating objects, initialize fields with default values when they don't need exteranl input for it.

--- wibbrlib/context.py
+++ wibbrlib/context.py
@@ -1,11 +1,16 @@
+import wibbrlib.config
+import wibbrlib.mapping
+import wibbrlib.obj
+
+
 class Context:
 
     def __init__(self):
-        self.config = None
+        self.config = wibbrlib.config.default_config()
         self.cache = None
         self.be = None
-        self.map = None
-        self.oq = None
+        self.map = wibbrlib.mapping.create()
+        self.oq = wibbrlib.obj.object_queue_create()
 
 
 def create():
