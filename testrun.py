b450c370d3f2bffd46add03c5af0501d98296768
Lars Wirzenius <liw@iki.fi>
2006-10-21 16:59:59 +0300

    Moved unit test files to unittests.

--- testrun.py
+++ testrun.py
@@ -3,7 +3,8 @@ import os
 import sys
 
 suite = unittest.TestSuite()
-for py in [py for py in os.listdir(".") if py.endswith("Tests.py")]:
+for py in [py for py in os.listdir("unittests") if py.endswith("Tests.py")]:
+    py = os.path.join("unittests", py)
     suite.addTest(unittest.defaultTestLoader.loadTestsFromName(py[:-3]))
 
 runner = unittest.TextTestRunner()
