cd361f2b4fb00b03d179c7f25038e3ae514216b6
Lars Wirzenius <liw@iki.fi>
2006-11-19 19:59:43 +0200

    New test case for sftp urls.

--- unittests/backendTests.py
+++ unittests/backendTests.py
@@ -18,6 +18,7 @@ class ParseStoreUrlTests(unittest.TestCase):
             ("sftp://host/", None, "host", None, "/"),
             ("sftp://host/foo", None, "host", None, "/foo"),
             ("sftp://user@host/foo", "user", "host", None, "/foo"),
+            ("sftp://host:22/foo", None, "host", 22, "/foo"),
         )
         for case in cases:
             user, host, port, path = obnam.backend.parse_store_url(case[0])
