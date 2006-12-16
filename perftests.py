92a43dca9d778cae75772ca97018b398aaf66e2f
Lars Wirzenius <liw@iki.fi>
2006-12-16 22:00:23 +0200

    Don't run with psyco.

--- perftests.py
+++ perftests.py
@@ -61,7 +61,7 @@ backup_methods = (
     ("rsync", rsync_method),
     ("obnam", obnam_method),
 #    ("obnam-profiled", profiled_obnam_method),
-    ("obnam-psyco", psyco_obnam_method),
+#    ("obnam-psyco", psyco_obnam_method),
 )
 
 
