9b1cd908b845d0090a38fcc6d1dbdbb49b98eaf5
Lars Wirzenius <liw@iki.fi>
2007-01-14 16:13:57 +0200

    Fixed defaultconfig so that it doesn't break tests.

--- obnam/defaultconfig.py
+++ obnam/defaultconfig.py
@@ -17,7 +17,7 @@ items = (
     ("backup", "gpg-home", "sample-gpg-home"),
     ("backup", "gpg-encrypt-to", "490C9ED1"),
     ("backup", "gpg-sign-with", "490C9ED1"),
-    ("backup", "no-gpg", ""),
+    ("backup", "no-gpg", "false"),
     ("backup", "exclude", ""),
     ("backup", "report-progress", "false"),
 )
