3eb7e2d01dcef5149071e2623f3cec0687f99425
Lars Wirzenius <liw@iki.fi>
2007-02-22 15:25:24 +0200

    Have --progress do a final report at the of the run.

--- cli.py
+++ cli.py
@@ -482,11 +482,11 @@ def main():
     else:
         raise UnknownCommandWord(command)
 
-    context.progress.clear()
     logging.info("Store I/O: %d kB read, %d kB written" % 
                  (obnam.backend.get_bytes_read(context.be) / 1024,
                   obnam.backend.get_bytes_written(context.be) / 1024))
     logging.info("Obnam finishing")
+    context.progress.final_report()
 
 
 if __name__ == "__main__":
