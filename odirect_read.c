3db635794637bb093b41c37e7615b582cdf45ae2
Lars Wirzenius <liw@iki.fi>
2007-02-23 12:09:57 +0200

    Workaround (hopefully working, but I'm unable to fully test right now) for O_DIRECT not working with Linux 2.4 kernels.

--- odirect_read.c
+++ odirect_read.c
@@ -54,6 +54,7 @@ int main(int argc, char **argv)
          * to read a device file, or a network filesystem, or something
          * else that won't work with O_DIRECT.
          */
+      open_without_o_direct:
         fd = open(argv[1], O_RDONLY | O_LARGEFILE);
     }
     if (fd == -1) {
@@ -64,7 +65,8 @@ int main(int argc, char **argv)
     
     /*
      * Linux 2.4 requires the buffer to be aligned at the filesystem
-     * block size, and also for the file offset to be a multiple of the
+     * block size, the transfer size needs to be a multiple of the block
+     * size, and also for the file offset to be a multiple of the
      * filesystem block size. We allocate a block that is a multiple of
      * said size and that should take care of things.
      */
@@ -74,12 +76,16 @@ int main(int argc, char **argv)
                 argv[1], errno, strerror(errno));
         return EXIT_FAILURE;
     }
+    fprintf(stderr, "block size is %lu\n", (unsigned long) sfs.f_bsize);
     
     /* 
      * In some quick testing, buffers much bigger than this did not make
      * things happen faster.
      */
     buf_size = sfs.f_bsize * 32;
+    fprintf(stderr, "buf_size is %ld\n", (long) buf_size);
+    fprintf(stderr, "buf_size % block size = %ld\n", 
+            (long) buf_size % (long) sfs.f_bsize);
 
     error = posix_memalign(&buf, sfs.f_bsize, buf_size);
     if (error != 0) {
@@ -87,12 +93,35 @@ int main(int argc, char **argv)
                 argv[0], error, strerror(error));
         return EXIT_FAILURE;
     }
+    fprintf(stderr, "buf = %p\n", (void *) buf);
+    fprintf(stderr, "buf % block size = %ld\n", 
+            (long) buf % (long) sfs.f_bsize);
     
+    ssize_t total = 0;
     for (;;) {
+        fprintf(stderr, "total = %ld, lseek = %ld\n", (long) total,
+                (long) lseek(fd, 0, SEEK_CUR));
         num_read = read(fd, buf, buf_size);
+        {
+            int e = errno;
+            fprintf(stderr, "num_read = %ld, errno = %d\n", 
+                    (long) num_read, e);
+            errno = e;
+        }
 
         if (num_read == -1 && errno == EINTR)
             continue;
+            
+        if (num_read == -1 && errno == EINVAL && total == 0) {
+            /* 
+             * For some reason, O_DIRECT doesn't seem to work under 2.4
+             * kernels anyway, at least not always. If so, it fails at
+             * the first read, so we restart from the beginning, without
+             * O_DIRECT.
+             */
+            (void) close(fd);
+            goto open_without_o_direct;
+        }
 
         if (num_read == -1) {
             fprintf(stderr, "%s: ERROR: Reading %s: %d: %s\n",
@@ -102,6 +131,8 @@ int main(int argc, char **argv)
 
         if (num_read == 0)
             break;
+
+        total += num_read;
 
         do {
             num_written = write(1, buf, num_read);
