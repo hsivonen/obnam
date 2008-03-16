87a667ed16f4bbca46fedd623410eb209ec70206
Lars Wirzenius <liw@gytha>
2008-03-16 15:16:39 +0200

    Added an object cache element to the context class. The next step is to get rid of obnam.io._object_cache global, for better testability.

--- obnam/contextTests.py
+++ obnam/contextTests.py
@@ -31,11 +31,12 @@ class ContextCreateTests(unittest.TestCase):
         attrs = [x for x in dir(context) if not x.startswith("_")]
         attrs.sort()
         self.failUnlessEqual(attrs, 
-            ["be", "cache", "config", "content_oq", "contmap", "map", "oq",
-             "progress"])
+            ["be", "cache", "config", "content_oq", "contmap", "map", "object_cache", 
+             "oq", "progress"])
         self.failUnlessEqual(context.be, None)
         self.failUnlessEqual(context.cache, None)
         self.failIfEqual(context.config, None)
         self.failIfEqual(context.map, None)
         self.failIfEqual(context.oq, None)
         self.failIfEqual(context.content_oq, None)
+        self.failUnlessEqual(context.object_cache, None)
