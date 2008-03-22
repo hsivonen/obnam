246e9957a29c1ef2aaca002da2f6c1037d2622cc
Lars Wirzenius <liw@gytha>
2008-03-22 17:21:58 +0200

    Updated the copyright year.

--- obnam/appTests.py
+++ obnam/appTests.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2006, 2007  Lars Wirzenius <liw@iki.fi>
+# Copyright (C) 2008  Lars Wirzenius <liw@iki.fi>
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
