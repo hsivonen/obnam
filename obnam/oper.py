1d8f531708cf7c597c6e94121492763b99a2d5fb
Lars Wirzenius <liw@gytha>
2008-03-23 13:19:16 +0200

    Started on Operation class.

--- obnam/oper.py
+++ obnam/oper.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2006  Lars Wirzenius <liw@iki.fi>
+# Copyright (C) 2008  Lars Wirzenius <liw@iki.fi>
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -15,33 +15,12 @@
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 
-"""The init file for the obnam module."""
+"""Obnam operations."""
 
 
-NAME = "obnam"
-VERSION = "0.9.0"
+class Operation:
 
+    name = None
 
-from exception import ObnamException
-
-import backend
-import cache
-import cfgfile
-import cmp
-import config
-import context
-import filelist
-import format
-import gpg
-import io
-import log
-import map
-import obj
-import progress
-import rsync
-import utils
-import varint
-import walk
-
-from app import Application
-from oper import Operation
+    def __init__(self, app, args):
+        pass
--- obnam/operTests.py
+++ obnam/operTests.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2006  Lars Wirzenius <liw@iki.fi>
+# Copyright (C) 2008  Lars Wirzenius <liw@iki.fi>
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -15,33 +15,20 @@
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 
-"""The init file for the obnam module."""
+"""Tests for obnam.oper."""
 
 
-NAME = "obnam"
-VERSION = "0.9.0"
+import unittest
+
+import obnam
 
 
-from exception import ObnamException
+class OperationTests(unittest.TestCase):
 
-import backend
-import cache
-import cfgfile
-import cmp
-import config
-import context
-import filelist
-import format
-import gpg
-import io
-import log
-import map
-import obj
-import progress
-import rsync
-import utils
-import varint
-import walk
+    def setUp(self):
+        context = obnam.context.Context()
+        app = obnam.Application(context)
+        self.op = obnam.Operation(app, ["pink", "pretty"])
 
-from app import Application
-from oper import Operation
+    def testNameIsNone(self):
+        self.failUnlessEqual(self.op.name, None)
