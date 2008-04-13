fc185e141d384d16bed20cfe9a590c964fac5571
Lars Wirzenius <liw@gytha>
2008-04-13 17:55:41 +0300

    Implemented putting objects into store and getting them back from there.

--- obnam/store.py
+++ obnam/store.py
@@ -18,7 +18,15 @@
 """Abstraction for storing backup data, for Obnam."""
 
 
+import logging
+
 import obnam
+
+
+class ObjectNotFoundInStore(obnam.exception.ObnamException):
+
+    def __init__(self, id):
+        self._msg = "Object %s not found in store" % id
 
 
 class Store:
@@ -44,8 +52,54 @@ class Store:
         
         """
         
-        host_id = self._context.config.get("backup", "host-id")
-        self._host = obnam.obj.HostBlockObject(host_id=host_id)
+        if not self._host:
+            host_block = obnam.io.get_host_block(self._context)
+            if host_block:
+                self._host = obnam.obj.create_host_from_block(host_block)
+            else:
+                id = self._context.config.get("backup", "host-id")
+                self._host = obnam.obj.HostBlockObject(host_id=id)
+        return self._host
+
+
+    def load_maps(self):
+        """Load non-content map blocks."""
+        ids = self._host.get_map_block_ids()
+        logging.info("Decoding %d mapping blocks" % len(ids))
+        obnam.io.load_maps(self._context, self._context.map, ids)
+
+    def load_content_maps(self):
+        """Load content map blocks."""
+        ids = self._host.get_contmap_block_ids()
+        logging.info("Decoding %d content mapping blocks" % len(ids))
+        obnam.io.load_maps(self._context, self._context.contmap, ids)
+
+    def _update_map_helper(self, map):
+        """Create new mapping blocks of a given kind, and upload them.
+        
+        Return list of block ids for the new blocks.
+
+        """
+
+        if obnam.map.get_new(map):
+            id = self._context.be.generate_block_id()
+            logging.debug("Creating mapping block %s" % id)
+            block = obnam.map.encode_new_to_block(map, id)
+            self._context.be.upload_block(id, block, True)
+            return [id]
+        else:
+            logging.debug("No new mappings, no new mapping block")
+            return []
+
+    def _update_maps(self):
+        """Create new object mapping blocks and upload them."""
+        logging.debug("Creating new mapping block for normal mappings")
+        return self._update_map_helper(self._context.map)
+
+    def _update_content_maps(self):
+        """Create new content object mapping blocks and upload them."""
+        logging.debug("Creating new mapping block for content mappings")
+        return self._update_map_helper(self._context.contmap)
 
     def commit_host_block(self):
         """Commit the current host block to the store.
@@ -53,6 +107,50 @@ class Store:
         If no host block exists, create one. If one already exists,
         update it with new info.
         
+        NOTE that after this operation the host block has changed,
+        and you need to call get_host_block again.
+        
+        """
+
+        obnam.io.flush_all_object_queues(self._context)
+    
+        logging.info("Creating new mapping blocks")
+        host = self.get_host_block()
+        map_ids = host.get_map_block_ids() + self._update_maps()
+        contmap_ids = (host.get_contmap_block_ids() + 
+                       self._update_content_maps())
+        
+        logging.info("Creating new host block")
+        gen_ids = host.get_generation_ids()
+        host2 = obnam.obj.HostBlockObject(host_id=host.get_id(), 
+                                          gen_ids=gen_ids, 
+                                          map_block_ids=map_ids,
+                                          contmap_block_ids=contmap_ids)
+        obnam.io.upload_host_block(self._context, host2.encode())
+
+        self._host = host2
+
+    def queue_object(self, object):
+        """Queue an object for upload to the store.
+        
+        It won't necessarily be committed (i.e., uploaded, etc) until
+        you call commit_host_block. Until it is committed, you may not
+        call get_object on it.
+        
         """
         
-        pass
+        obnam.io.enqueue_object(self._context, self._context.oq,
+                                self._context.map, object.get_id(), 
+                                object.encode(), True)
+
+    def get_object(self, id):
+        """Get an object from the store.
+        
+        If the object cannot be found, raise an exception.
+        
+        """
+
+        object = obnam.io.get_object(self._context, id)
+        if object:
+            return object
+        raise ObjectNotFoundInStore(id)
--- obnam/storeTests.py
+++ obnam/storeTests.py
@@ -18,6 +18,7 @@
 """Unit tests for abstraction for storing backup data, for Obnam."""
 
 
+import shutil
 import unittest
 
 import obnam
@@ -27,8 +28,15 @@ class StoreTests(unittest.TestCase):
 
     def setUp(self):
         context = obnam.context.Context()
+        context.cache = obnam.cache.Cache(context.config)
         context.be = obnam.backend.init(context.config, context.cache)
         self.store = obnam.Store(context)
+
+    def tearDown(self):
+        shutil.rmtree(self.store._context.config.get("backup", "store"),
+                      ignore_errors=True)
+        shutil.rmtree(self.store._context.config.get("backup", "cache"),
+                      ignore_errors=True)
 
     def testReturnsNoneWhenNoHostBlockExists(self):
         self.failUnlessEqual(self.store.get_host_block(), None)
@@ -48,3 +56,21 @@ class StoreTests(unittest.TestCase):
         store.fetch_host_block()
         host = store.get_host_block()
         self.failUnless(isinstance(host, obnam.obj.HostBlockObject))
+
+    def testGettingNonExistentObjectRaisesException(self):
+        self.failUnlessRaises(obnam.exception.ObnamException,
+                              self.store.get_object, "pink")
+
+    def testAddsObjectToStore(self):
+        o = obnam.obj.GenerationObject(id="pink")
+        self.store.fetch_host_block()
+        self.store.queue_object(o)
+        self.store.commit_host_block()
+        
+        context = obnam.context.Context()
+        context.cache = obnam.cache.Cache(context.config)
+        context.be = obnam.backend.init(context.config, context.cache)
+        store = obnam.Store(context)
+        store.fetch_host_block()
+        store.load_maps()
+        self.failUnless(store.get_object(o.get_id()))
