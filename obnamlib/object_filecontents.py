ef2705fd4c63555b63cf78efa1c3c2b429d66378
Lars Wirzenius <liw@liw.fi>
2008-12-07 23:30:29 +0200

    Added FileContents object.

---
+++ obnamlib/object_filecontents.py
@@ -0,0 +1,35 @@
+# Copyright (C) 2008  Lars Wirzenius <liw@liw.fi>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+
+import obnamlib
+
+
+class FileContents(obnamlib.Object):
+
+    """Store the full contents of a file."""
+
+    def __init__(self, id):
+        obnamlib.Object.__init__(self, id=id, kind=obnamlib.FILECONTENTS)
+
+    @property
+    def part_ids(self):
+        return [c.string for c in self.find(kind=obnamlib.FILEPARTREF)]
+
+    def add(self, ref):
+        c = obnamlib.Component(kind=obnamlib.FILEPARTREF)
+        c.string = ref
+        self.components.append(c)
