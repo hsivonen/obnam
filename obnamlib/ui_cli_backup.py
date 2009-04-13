ffb0d4b80873f6346e1e95fa1f0f0621dd55162d
Lars Wirzenius <liw@gytha>
2009-04-13 15:42:52 +0300

    Re-use existing filegroups, files, when possible. This doesn't quite work yet: need to store parent directories of roots that get backed up.

--- obnamlib/ui_cli_backup.py
+++ obnamlib/ui_cli_backup.py
@@ -82,14 +82,73 @@ class BackupCommand(object):
         self.store.put_object(dir)
         return dir
 
+    def find_reusable_filegroups(self, prevdir, dirname, filenames): # pragma: no cover
+        """Find filegroups that can be re-used from previous generation.
+        
+        Return list of filegroups and list of filenames NOT in those groups.
+        
+        """
+        
+        filegroups = []
+        for fgref in prevdir.fgrefs:
+            fg = self.store.get_object(self.host, fgref)
+            for name in fg.names:
+                if (name not in filenames or
+                    not self.same_file(dirname, name, fg)):
+                    break
+            else:
+                filegroups.append(fg)
+                filenames = [x for x in filenames if x not in fg.names]
+
+        return filegroups, filenames
+
+    def same_file(self, dirname, basename, fg): # pragma: no cover
+        """Is the file on disk the same as in the filegroup?"""
+        fullname = os.path.join(dirname, basename)
+        st = self.fs.lstat(fullname)
+        return self.same_stat(st, fg.get_stat(name))
+
+    def same_filegroups(self, prevdir, filegroups): # pragma: no cover
+        """Does prevdir have exactly those filegroups?"""
+        return set(prevdir.fgrefs) == set(x.id for x in filegroups)
+        
+    def same_subdirs(self, prevdir, subdirs): # pragma: no cover
+        """Does prevdir have exactly those sub-directories?"""
+        return set(prevdir.dirrefs) == set(x.id for x in subdirs)
+
+    def same_stat(self, stat1, stat2): # pragma: no cover
+        """Are two stats identical for all the relevant values?"""
+        return (stat1.st_mode  == stat2.st_mode and
+                stat1.st_dev   == stat2.st_dev and
+                stat1.st_nlink == stat2.st_nlink and
+                stat1.st_uid   == stat2.st_uid and
+                stat1.st_gid   == stat2.st_gid and
+                stat1.st_size  == stat2.st_size and
+                stat1.st_mtime == stat2.st_mtime)
+
     def backup_existing_dir(self, prevdir, relative_path, st, subdirs, 
                             filenames): # pragma: no cover
         """Back up a directory that exists in the previous generation."""
 
-        return self.backup_new_dir(relative_path, st, subdirs, filenames)
+        logging.debug("Directory EXISTS in previous gen: %s" % relative_path)
+
+        filegroups, filenames = self.find_reusable_filegroups(prevdir, 
+                                                              relative_path,
+                                                              filenames)
+        if (not filenames and
+            self.same_filegroups(prevdir, filegroups) and
+            self.same_subdirs(prevdir, subdirs) and
+            self.same_stat(prevdir.stat, st)):
+            return prevdir
+        else:
+            if filenames:
+                fullnames = [os.path.join(relative_path, x) for x in filenames]
+                filegroups += self.backup_new_files_as_groups(fullnames)
+            return self.new_dir(relative_path, st, subdirs, filegroups)
 
     def backup_new_dir(self, relative_path, st, subdirs, filenames):
         """Back up a new directory."""
+        logging.debug("Directory is NEW: %s" % relative_path)
         fullnames = [os.path.join(relative_path, x) for x in filenames]
         filegroups = self.backup_new_files_as_groups(fullnames)
         return self.new_dir(relative_path, st, subdirs, filegroups)
@@ -110,11 +169,14 @@ class BackupCommand(object):
 
         """
 
+        logging.debug("Backing up directory %s" % relative_path)
+
         if lstat is None: # pragma: no cover
             lstat = self.fs.lstat
         st = lstat(relative_path)
 
         prevdir = self.get_dir_in_prevgen(relative_path)
+        logging.debug("backup_dir: prevdir=%s" % repr(prevdir))
         if prevdir: # pragma: no cover
             dir = self.backup_existing_dir(prevdir, relative_path, st,
                                            subdirs, filenames)
@@ -125,6 +187,8 @@ class BackupCommand(object):
 
     def backup_recursively(self, root):
         """Back up a directory, recursively."""
+        
+        logging.debug("Backing up recursively: %s" % root)
 
         # We traverse the directory tree depth-first. When we get to a
         # directory, we need to know its subdirectories. We keep them
@@ -161,17 +225,21 @@ class BackupCommand(object):
         return gen
 
     def backup(self, host_id, roots):
-        host = self.store.get_host(host_id)
-        if host.genrefs: # pragma: no cover
-            prevgenref = host.genrefs[-1]
-            prevgen = self.store.get_object(host, prevgenref)
-            self.prevgen_lookupper = obnamlib.Lookupper(self.store, host, 
+        logging.debug("Backing up: host %s, roots %s" % 
+                      (host_id, " ".join(roots)))
+        self.host = self.store.get_host(host_id)
+        if self.host.genrefs: # pragma: no cover
+            prevgenref = self.host.genrefs[-1]
+            logging.debug("Found previous generation %s" % prevgenref)
+            prevgen = self.store.get_object(self.host, prevgenref)
+            self.prevgen_lookupper = obnamlib.Lookupper(self.store, self.host, 
                                                         prevgen)
         else:
+            logging.debug("Using DummyLookupper")
             self.prevgen_lookupper = DummyLookupper()
         gen = self.backup_generation(roots)
-        host.genrefs.append(gen.id)
-        self.store.commit(host)
+        self.host.genrefs.append(gen.id)
+        self.store.commit(self.host)
 
     def __call__(self, config, args): # pragma: no cover
         host_id = args[0]
