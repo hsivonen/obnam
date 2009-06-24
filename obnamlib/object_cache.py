fbd8e6f03b2d52a28c7b4a50cf0075cada23fea1
Lars Wirzenius <liw@liw.fi>
2009-06-24 08:43:05 +0300

    Wrote another ObjectCache implementation and a benchmarking script.

--- obnamlib/object_cache.py
+++ obnamlib/object_cache.py
@@ -21,9 +21,12 @@ import os
 import obnamlib
 
 
-class ObjectCache(object):
+class SimplisticObjectCache(object):
 
     """Cache objects in memory."""
+    
+    # This is a very simplistic LRU implementation: it keeps the objects
+    # in a list in the order in which they have last been used.
     
     def __init__(self):
         self.dict = {}
@@ -53,4 +56,52 @@ class ObjectCache(object):
             return self.dict[objid]
         else:
             return None
+
+
+class CounterObjectCache(object):
+
+    """Cache objects in memory."""
+    
+    # This simulates a clock with a counter: each object is paired with a
+    # value and for each use, the value is set to the current value of the
+    # counter. When it is time to forget, the lowest counter is removed.
+    
+    def __init__(self):
+        self.dict = {}
+        self.counter = 0
+        # Compute a default max cache size by assuming a one megabyte
+        # block size and a 64 byte object size.
+        self.max = 1000 * 1000 / 64
+        
+    def put(self, obj):
+        if obj.id in self.dict:
+            self.dict[obj.id][0] = self.counter
+        else:
+            # We put in a pair as a _list_, not a tuple, so we can 
+            # modify it in place, for speed.
+            self.dict[obj.id] = [self.counter, obj]
+        self.counter += 1
+        self.forget()
+
+    def forget(self):
+        # We _should_ only be removing one item, but just in case we remove
+        # more, we loop rather than test once. Since this should never
+        # happen, it should be very fast.
+        while len(self.dict) > self.max:
+            keys = self.dict.keys()
+            counters = [(self.dict[objid][0], objid) for objid in keys]
+            counters.sort()
+            del self.dict[counters[0][1]]
+        
+    def get(self, objid):
+        if objid in self.dict:
+            pair = self.dict[objid]
+            pair[0] = self.counter
+            self.counter += 1
+            return pair[1]
+        else:
+            return None
+
+
+ObjectCache = CounterObjectCache
 
