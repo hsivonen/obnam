1cf361656c25a75a6b5bf5423d6a8f07d4073ffd
Lars Wirzenius <liw@liw.fi>
2009-11-30 20:32:56 +0200

    Fixed handling of default values for config settings. We need to copy them when we store them in settings and in the option parser. Otherwise, for mutable objects, the same instance of the value is used, and when, say, the settings are modified, the parsed options' value is also changed. This resulted in an infinite loop in the Configuration.loop method during unit tests.

--- obnamlib/cfg.py
+++ obnamlib/cfg.py
@@ -14,6 +14,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
+import copy
 import optparse
 
 
@@ -72,13 +73,14 @@ class Configuration(object):
         self.args = []
 
     def new_setting(self, kind, names, help, action, value):
-        setting = Setting(kind, value)
+        setting = Setting(kind, copy.copy(value))
         for name in names:
             self.settings[name] = setting
 
         optnames = ['-%s' % name for name in names if len(name) == 1]
         optnames += ['--%s' % name for name in names if len(name) > 1]
-        self.parser.add_option(*optnames, action=action, help=help)
+        self.parser.add_option(*optnames, action=action, help=help,
+                               default=copy.copy(value))
 
     def new_boolean(self, names, help):
         self.new_setting('int', names, help, 'store_true', False)
