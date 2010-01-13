071d628daf1ec68ebf07c4db7403ba01fc59bf27
Lars Wirzenius <liw@liw.fi>
2010-01-13 14:16:34 +1300

    Add a preliminary test module for store.py.

---
+++ obnamlib/store_tests.py
@@ -0,0 +1,36 @@
+# Copyright (C) 2010  Lars Wirzenius
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+import shutil
+import tempfile
+import unittest
+
+import obnamlib
+
+
+class StoreTests(unittest.TestCase):
+
+    def setUp(self):
+        self.tempdir = tempfile.mkdtemp()
+        self.fs = obnamlib.LocalFS(self.tempdir)
+        self.store = obnamlib.Store(self.fs)
+
+    def tearDown(self):
+        shutil.rmtree(self.tempdir)
+
+    def test_foo(self):
+        self.assert_(False)
+
