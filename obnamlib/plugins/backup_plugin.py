7ad657e3861ae82f78f283988613532fa1177940
Lars Wirzenius <liw@liw.fi>
2010-01-19 18:38:06 +1300

    Add docstrings to methods.
    Remove FIXME comments; the proper place for that information
    is in a centralized place (currently my personal GTD system,
    but eventually a bug tracker).

--- obnamlib/plugins/backup_plugin.py
+++ obnamlib/plugins/backup_plugin.py
@@ -64,6 +64,12 @@ class BackupPlugin(obnamlib.ObnamPlugin):
             self.backup_file(root)
 
     def backup_file(self, root):
+        '''Back up a non-directory.
+        
+        If it is a regular file, also back up its contents.
+        
+        '''
+        
         metadata = obnamlib.read_metadata(self.fs, root)
         self.app.hooks.call('progress-found-file', root, metadata.st_size)
         self.store.create(root, metadata)
@@ -71,9 +77,7 @@ class BackupPlugin(obnamlib.ObnamPlugin):
             self.backup_file_contents(root)
 
     def backup_file_contents(self, filename):
-        # FIXME: This should try to re-use chunks.
-        # FIXME: This should use/create chunk groups when possible.
-        # FIXME: This should handle chunk/chunk group collisions
+        '''Back up contents of a regular file.'''
         chunkids = []
         f = self.fs.open(filename, 'r')
         while True:
@@ -88,8 +92,7 @@ class BackupPlugin(obnamlib.ObnamPlugin):
         self.store.set_file_chunks(filename, chunkids)
 
     def backup_dir(self, root):
-        # FIXME: This should attempt to reuse existing data rather than
-        # removing the directory that was in the previous generation.
+        '''Back up a directory, and everything in it.'''
         try:
             self.store.get_metadata(self.store.new_generation, root)
         except obnamlib.Error:
