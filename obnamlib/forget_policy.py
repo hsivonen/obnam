0368624e17e678035ee6623788f127096febb363
Lars Wirzenius <liw@liw.fi>
2010-02-09 22:14:02 +1300

    Implement matching of forget policy against a list of generations and their timestamps.

--- obnamlib/forget_policy.py
+++ obnamlib/forget_policy.py
@@ -72,3 +72,49 @@ class ForgetPolicy(object):
         result.update((x, 0) for x, y in result.iteritems() if y is None)
         return result
 
+    def last_in_each_period(self, period, genlist):
+        formats = {
+            'hourly': '%Y-%m-%d %H',
+            'daily': '%Y-%m-%d',
+            'weekly': '%Y-%W',
+            'monthly': '%Y-%m',
+            'yearly': '%Y',
+        }
+    
+        matches = []
+        for genid, dt in genlist:
+            formatted = dt.strftime(formats[period])
+            if not matches:
+                matches.append((genid, formatted))
+            elif matches[-1][1] == formatted:
+                matches[-1] = (genid, formatted)
+            else:
+                matches.append((genid, formatted))
+        return [genid for genid, formatted in matches]
+
+    def match(self, rules, genlist):
+        '''Match a parsed ruleset against a list of generations and times.
+        
+        The ruleset should be of the form returned by the parse method.
+        
+        genlist should be a list of generation identifiers and timestamps.
+        Identifiers can be anything, timestamps should be an instance
+        of datetime.datetime, with no time zone (it is ignored).
+        
+        genlist should be in ascending order by time: oldest one first.
+        
+        Return value is all those pairs from genlist that should be
+        kept (i.e., which match the rules).
+        
+        '''
+
+        result_ids = set()
+        for period in rules:
+            genids = self.last_in_each_period(period, genlist)
+            if rules[period]:
+                for genid in genids[-rules[period]:]:
+                    result_ids.add(genid)
+
+        return [(genid, dt) for genid, dt in genlist
+                if genid in result_ids]
+
--- obnamlib/forget_policy_tests.py
+++ obnamlib/forget_policy_tests.py
@@ -14,12 +14,13 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
+import datetime
 import unittest
 
 import obnamlib
 
 
-class ForgetPolicyTests(unittest.TestCase):
+class ForgetPolicyParseTests(unittest.TestCase):
 
     def setUp(self):
         self.fp = obnamlib.ForgetPolicy()
@@ -51,4 +52,67 @@ class ForgetPolicyTests(unittest.TestCase):
                            'weekly': 3,
                            'monthly': 4,
                            'yearly': 255 })
+
+
+times = []
+for year in range(2009, 2010):
+    for month in range(1, 13):
+        for day in range(1, 29):
+            for hour in range(24):
+                for minute in range(58, 60):
+                    dt = datetime.datetime(year, month, day,
+                                            hour, minute)
+                    times.append(dt)
+enumerated_times = list(enumerate(times))
+
+
+class ForgetPolicyMatchTests(unittest.TestCase):
+
+    def setUp(self):
+        self.fp = obnamlib.ForgetPolicy()
+
+    def match(self, spec):
+        rules = self.fp.parse(spec)
+        return [time 
+                for i, time in self.fp.match(rules, enumerated_times)]
+
+    def test_hourly_matches(self):
+        self.assertEqual(self.match('1h'),
+                         [dt for dt in times if dt.minute == 59][-1:])
+
+    def test_daily_matches(self):
+        self.assertEqual(self.match('1d'),
+                         [dt for dt in times 
+                          if dt.hour == 23 and dt.minute == 59][-1:])
+
+    # Not testing weekly matching, since I can't figure out to make
+    # a sensible test case right now.
+
+    def test_monthly_matches(self):
+        self.assertEqual(self.match('1m'),
+                         [dt for dt in times 
+                          if dt.day == 28 and 
+                             dt.hour == 23 and 
+                             dt.minute == 59][-1:])
+
+    def test_yearly_matches(self):
+        self.assertEqual(self.match('1y'),
+                         [dt for dt in times 
+                          if dt.month == 12 and
+                             dt.day == 28 and 
+                             dt.hour == 23 and 
+                             dt.minute == 59][-1:])
+
+    def test_hourly_and_daily_match_together(self):
+        d1h0m0 = datetime.datetime(2000, 1, 1, 0, 0)
+        d1h0m1 = datetime.datetime(2000, 1, 1, 0, 1)
+        d2h0m0 = datetime.datetime(2000, 1, 2, 0, 0)
+        d2h0m1 = datetime.datetime(2000, 1, 2, 0, 1)
+        d3h0m0 = datetime.datetime(2000, 1, 3, 0, 0)
+        d3h0m1 = datetime.datetime(2000, 1, 3, 0, 1)
+        genlist = list(enumerate([d1h0m0, d1h0m1, d2h0m0, d2h0m1,
+                                  d3h0m0, d3h0m1]))
+        rules = self.fp.parse('1h,2d')
+        self.assertEqual([dt for genid, dt in self.fp.match(rules, genlist)],
+                         [d2h0m1, d3h0m1])
 
