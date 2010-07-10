1f21a4f9cd951e5f73ba9a0edd771d6ad793fc42
Lars Wirzenius <liw@liw.fi>
2010-07-11 11:37:57 +1200

    Fix backup plugin to use bytes_written.

--- obnamlib/plugins/backup_plugin.py
+++ obnamlib/plugins/backup_plugin.py
@@ -93,13 +93,13 @@ class BackupPlugin(obnamlib.ObnamPlugin):
                     self.app.hooks.call('error-message', 
                                         'Could not back up %s: %s' %
                                         (pathname, e.strerror))
-                if storefs.written >= self.app.config['checkpoint']:
+                if storefs.bytes_written >= self.app.config['checkpoint']:
                     logging.debug('Making checkpoint')
                     self.backup_parents('.')
                     self.store.commit_host(checkpoint=True)
                     self.store.lock_host(hostname)
                     self.store.start_generation()
-                    storefs.written = 0
+                    storefs.bytes_written = 0
 
             self.backup_parents('.')
 
