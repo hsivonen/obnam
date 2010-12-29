198348e49af5abc2e5edb9d5329fcbb26f16d151
Lars Wirzenius <liw@liw.fi>
2010-12-29 20:48:35 +0000

    Prepare for release of version 0.14.

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ from distutils.core import setup, Extension
 import glob
 
 setup(name='obnam',
-      version='0.13',
+      version='0.14',
       description='Backup software',
       author='Lars Wirzenius',
       author_email='liw@liw.fi',
