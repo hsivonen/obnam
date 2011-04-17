b6cfdcfcaa83f0b9d144338c4f51f103dc6e340e
Lars Wirzenius <liw@liw.fi>
2011-04-17 10:33:39 +0100

    Fix tests that fail because symmetric keys are now in hex.
    Dammit, I need someone to set up a PQM for Obnam.

--- obnamlib/encryption_tests.py
+++ obnamlib/encryption_tests.py
@@ -41,12 +41,12 @@ class SymmetricEncryptionTests(unittest.TestCase):
     def test_generates_key_of_correct_length(self):
         numbits = 16
         key = obnamlib.generate_symmetric_key(numbits, filename='/dev/zero')
-        self.assertEqual(len(key) * 8, numbits)
+        self.assertEqual(len(key) * 8 / 2, numbits) # /2 for hex encoding
 
     def test_generates_key_with_size_rounded_up(self):
         numbits = 15
         key = obnamlib.generate_symmetric_key(numbits, filename='/dev/zero')
-        self.assertEqual(len(key), 2)
+        self.assertEqual(len(key)/2, 2) # /2 for hex encoding
 
     def test_encrypts_into_different_string_than_cleartext(self):
         cleartext = 'hello world'
