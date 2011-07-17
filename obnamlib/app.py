72c742a24c5d86744f09a364df873cc2d16431ec
Lars Wirzenius <liw@liw.fi>
2011-07-17 21:32:27 +0100

    Move things about a bit to fit cliapp's structure a bit better.
    Still ugly.

--- obnamlib/app.py
+++ obnamlib/app.py
@@ -28,22 +28,6 @@ class App(cliapp.Application):
 
     '''Main program for backup program.'''
     
-    def __init__(self):
-        cliapp.Application.__init__(self)
-
-        self.setup_config()        
-
-        self.pm = obnamlib.PluginManager()
-        self.pm.locations = [self.plugins_dir()]
-        self.pm.plugin_arguments = (self,)
-        
-        self.interp = obnamlib.Interpreter()
-        self.register_command = self.interp.register
-
-        self.setup_hooks()
-
-        self.fsf = obnamlib.VfsFactory()
-
     @property
     def default_config_file(self):
         if os.getuid() == 0:
@@ -53,7 +37,7 @@ class App(cliapp.Application):
                      os.path.expanduser('~/.cache'))
             return os.path.join(cache, 'obnam', 'obnam.log')
 
-    def setup_config(self):
+    def add_settings(self):
         self.config = self.settings
 
         self.config.string(['repository', 'r'], 'name of backup repository')
@@ -96,6 +80,21 @@ class App(cliapp.Application):
                                 'add to filename patters for which trace '
                                 'debugging logging happens')
         
+        self.pm = obnamlib.PluginManager()
+        self.pm.locations = [self.plugins_dir()]
+        self.pm.plugin_arguments = (self,)
+        
+        self.interp = obnamlib.Interpreter()
+        self.register_command = self.interp.register
+
+        self.setup_hooks()
+
+        self.fsf = obnamlib.VfsFactory()
+
+        self.pm.load_plugins()
+        self.pm.enable_plugins()
+        self.hooks.call('plugins-loaded')
+
     def deduce_client_name(self):
         return socket.gethostname()
 
@@ -115,56 +114,8 @@ class App(cliapp.Application):
     def plugins_dir(self):
         return os.path.join(os.path.dirname(obnamlib.__file__), 'plugins')
 
-    def rotate_logs(self, filename, keep):
-        def rename(old_suffix, counter):
-            new_suffix = '.%d' % counter
-            if os.path.exists(filename + new_suffix):
-                if counter < keep:
-                    rename(new_suffix, counter + 1)
-                else:
-                    os.remove(filename + new_suffix)
-            os.rename(filename + old_suffix, filename + new_suffix)
-        rename('', 0)
-
-    def setup_logging(self):
-        log_filename = self.config['log']
-        log_max = self.config['log-max']
-        log_keep = int(self.config['log-keep'])
-        if (os.path.exists(log_filename) and 
-            os.path.getsize(log_filename) > log_max):
-            self.rotate_logs(log_filename, log_keep)
-        if os.getuid() != 0:
-            log_dir = os.path.dirname(log_filename)
-            if log_dir and not os.path.exists(log_dir):
-                os.makedirs(log_dir)
-        
-        formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
-        handler = logging.FileHandler(self.config['log'])
-        handler.setFormatter(formatter)
-        logger = logging.getLogger()
-        levels = {
-            'debug': logging.DEBUG,
-            'info': logging.INFO,
-            'warning': logging.WARNING,
-            'error': logging.ERROR,
-            'critical': logging.CRITICAL,
-        }
-        level_name = self.config['log-level']
-        level = levels.get(level_name.lower(), logging.DEBUG)
-        logger.setLevel(level)
-        logger.addHandler(handler)
-        
-        for pattern in self.config['trace']:
-            tracing.trace_add_pattern(pattern)
-        
-    def run(self):
-        self.pm.load_plugins()
-        self.pm.enable_plugins()
-        self.hooks.call('plugins-loaded')
-        self.settings.load_configs()
-        args = self.settings.parse_args(sys.argv[1:])
+    def process_args(self, args):
         self.hooks.call('config-loaded')
-        self.setup_logging()
         logging.info('Obnam %s starts' % obnamlib.version)
         if args:
             logging.info('Executing command: %s' % args[0])
