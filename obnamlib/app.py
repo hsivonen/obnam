aad5eb9e4e9904fcdecae5363628f71aa2f39200
Lars Wirzenius <liw@liw.fi>
2011-07-17 13:18:14 +0100

    Start use of cliapp.

--- obnamlib/app.py
+++ obnamlib/app.py
@@ -14,6 +14,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
+import cliapp
 import logging
 import os
 import socket
@@ -22,11 +23,12 @@ import tracing
 import obnamlib
 
 
-class App(object):
+class App(cliapp.Application):
 
     '''Main program for backup program.'''
     
     def __init__(self):
+        cliapp.Application.__init__(self)
 
         self.setup_config()        
 
