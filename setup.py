d858f913f60f2b58f6541915484f5c2d6788ca68
Lars Wirzenius <liw@liw.fi>
2011-08-03 19:28:10 +0100

    Bump version number.

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ from distutils.core import setup, Extension
 import glob
 
 setup(name='obnam',
-      version='0.18',
+      version='0.19',
       description='Backup software',
       author='Lars Wirzenius',
       author_email='liw@liw.fi',
