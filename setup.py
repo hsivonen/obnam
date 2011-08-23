dfbdd0009ccccf276e88121c3c545a7edecbdc9e
Lars Wirzenius <liw@liw.fi>
2011-08-23 17:28:02 +0100

    Update copyright years.

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-# Copyright (C) 2008  Lars Wirzenius <liw@liw.fi>
+# Copyright (C) 2008-2011  Lars Wirzenius <liw@liw.fi>
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
