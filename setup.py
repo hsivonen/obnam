f081fff54826fab68b90aed779ecd3ca42daab4e
Lars Wirzenius <liw@liw.fi>
2011-08-25 08:29:10 +0100

    Bump version number.

--- setup.py
+++ setup.py
@@ -90,7 +90,7 @@ class Check(Command):
 
 
 setup(name='obnam',
-      version='0.21',
+      version='0.22',
       description='Backup software',
       author='Lars Wirzenius',
       author_email='liw@liw.fi',
