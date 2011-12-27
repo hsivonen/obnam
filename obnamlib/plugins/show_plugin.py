37cb0a9a6ee59761e54e8cd0ed2d57729e87a68f
Lars Wirzenius <liw@liw.fi>
2011-12-27 20:13:41 +0000

    break long lines so they fit within 80 cols

--- obnamlib/plugins/show_plugin.py
+++ obnamlib/plugins/show_plugin.py
@@ -40,21 +40,26 @@ class ShowPlugin(obnamlib.ObnamPlugin):
         self.app.add_subcommand('generations', self.generations)
         self.app.add_subcommand('genids', self.genids)
         self.app.add_subcommand('ls', self.ls, arg_synopsis='[GENERATION]...')
-        self.app.add_subcommand('nagios-last-backup-age', self.nagios_last_backup_age)
+        self.app.add_subcommand('nagios-last-backup-age', 
+                                self.nagios_last_backup_age)
 
         self.app.settings.string(['warn-age'],
-                                  'for nagios-last-backup-age: maximum age (by '+
-                                  'default in hours) for the most recent backup '+
-                                  'before status is warning.  Accepts one char '+
-                                  'unit specifier (s,m,h,d for seconds, minutes, '+
-                                  'hours, and days.', metavar='AGE',
+                                 'for nagios-last-backup-age: maximum age (by 
+                                    'default in hours) for the most recent '
+                                    'backup before status is warning. '
+                                    'Accepts one char unit specifier '
+                                    '(s,m,h,d for seconds, minutes, hours, '
+                                    'and days.', 
+                                  metavar='AGE',
                                   default=obnamlib.DEFAULT_NAGIOS_WARN_AGE)
         self.app.settings.string(['critical-age'],
-                                  'for nagios-last-backup-age: maximum age (by '+
-                                  'default in hours) for the most recent backup '+
-                                  'before statis is critical.  Accepts one char '+
-                                  'unit specifier (s,m,h,d for seconds, minutes, '+
-                                  'hours, and days.', metavar='AGE',
+                                 'for nagios-last-backup-age: maximum age '
+                                    '(by default in hours) for the most '
+                                    'recent backup before statis is critical. '
+                                    'Accepts one char unit specifier '
+                                    '(s,m,h,d for seconds, minutes, hours, '
+                                    'and days.', 
+                                  metavar='AGE',
                                   default=obnamlib.DEFAULT_NAGIOS_WARN_AGE)
 
     def open_repository(self):
