9b737e8fdcbaa829e0ea58998b6d26334b7e5dfe
Lars Wirzenius <liw@liw.fi>
2012-05-11 00:22:35 +0100

    Fix version number in setup.py

--- setup.py
+++ setup.py
@@ -125,7 +125,7 @@ class Check(Command):
 
 
 setup(name='obnam',
-      version='0.26',
+      version='0.28',
       description='Backup software',
       author='Lars Wirzenius',
       author_email='liw@liw.fi',
