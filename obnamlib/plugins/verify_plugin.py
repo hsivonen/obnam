990ef2be5fb66e1922eb8a7efb8ef29195f45c1f
Lars Wirzenius <liw@liw.fi>
2013-12-09 17:46:36 +0000

    Convert verify plugin to RepositoryInterface

--- obnamlib/plugins/verify_plugin.py
+++ obnamlib/plugins/verify_plugin.py
@@ -63,8 +63,8 @@ class VerifyPlugin(obnamlib.ObnamPlugin):
             args = ['/']
         logging.debug('verifying what: %s' % repr(args))
 
-        self.repo = self.app.open_repository()
-        self.repo.open_client(self.app.settings['client-name'])
+        self.repo = self.app.get_repository_object()
+        client_name = self.app.settings['client-name']
         self.fs = self.app.fsf.new(args[0])
         self.fs.connect()
         t = urlparse.urlparse(args[0])
@@ -74,7 +74,9 @@ class VerifyPlugin(obnamlib.ObnamPlugin):
         self.fs.reinit(root_url)
 
         self.failed = False
-        gen = self.repo.genspec(self.app.settings['generation'][0])
+        gen_id = self.repo.interpret_generation_spec(
+            client_name,
+            self.app.settings['generation'][0])
 
         self.app.ts['done'] = 0
         self.app.ts['total'] = 0
@@ -89,17 +91,19 @@ class VerifyPlugin(obnamlib.ObnamPlugin):
         num_randomly = self.app.settings['verify-randomly']
         if num_randomly == 0:
             self.app.ts['total'] = \
-                self.repo.client.get_generation_file_count(gen)
-            for filename, metadata in self.walk(gen, args):
+                self.repo.get_generation_key(
+                gen_id, obnamlib.REPO_GENERATION_FILE_COUNT)
+            for filename, metadata in self.walk(gen_id, args):
                 self.app.ts['filename'] = filename
                 try:
-                    self.verify_metadata(gen, filename, metadata)
+                    self.verify_metadata(gen_id, filename, metadata)
                 except Fail, e:
                     self.log_fail(e)
                 else:
                     if metadata.isfile():
                         try:
-                            self.verify_regular_file(gen, filename, metadata)
+                            self.verify_regular_file(
+                                gen_id, filename, metadata)
                         except Fail, e:
                             self.log_fail(e)
                 self.app.ts['done'] += 1
@@ -108,7 +112,7 @@ class VerifyPlugin(obnamlib.ObnamPlugin):
             self.app.ts['total'] = num_randomly
             self.app.ts.notify('finding all files to choose randomly')
             filenames = [filename
-                         for filename, metadata in self.walk(gen, args)
+                         for filename, metadata in self.walk(gen_id, args)
                          if metadata.isfile()]
             chosen = []
             for i in range(min(num_randomly, len(filenames))):
@@ -117,16 +121,16 @@ class VerifyPlugin(obnamlib.ObnamPlugin):
                 chosen.append(filename)
             for filename in chosen:
                 self.app.ts['filename'] = filename
-                metadata = self.repo.get_metadata(gen, filename)
+                metadata = self.construct_metadata_object(gen_id, filename)
                 try:
-                    self.verify_metadata(gen, filename, metadata)
-                    self.verify_regular_file(gen, filename, metadata)
+                    self.verify_metadata(gen_id, filename, metadata)
+                    self.verify_regular_file(gen_id, filename, metadata)
                 except Fail, e:
                     self.log_fail(e)
                 self.app.ts['done'] += 1
 
         self.fs.close()
-        self.repo.fs.close()
+        self.repo.close()
         self.app.ts.finish()
 
         if self.failed:
@@ -142,7 +146,7 @@ class VerifyPlugin(obnamlib.ObnamPlugin):
             self.app.ts.notify(msg)
         self.failed = True
 
-    def verify_metadata(self, gen, filename, backed_up):
+    def verify_metadata(self, gen_id, filename, backed_up):
         try:
             live_data = obnamlib.read_metadata(self.fs, filename)
         except OSError, e:
@@ -152,13 +156,13 @@ class VerifyPlugin(obnamlib.ObnamPlugin):
             v2 = getattr(live_data, field)
             if v1 != v2:
                 raise Fail(filename,
-                            'metadata change: %s (%s vs %s)' % (field, v1, v2))
+                           'metadata change: %s (%s vs %s)' % (field, v1, v2))
 
-    def verify_regular_file(self, gen, filename, metadata):
+    def verify_regular_file(self, gen_id, filename, metadata):
         logging.debug('verifying regular %s' % filename)
         f = self.fs.open(filename, 'r')
 
-        chunkids = self.repo.get_file_chunks(gen, filename)
+        chunkids = self.repo.get_file_chunk_ids(gen_id, filename)
         if not self.verify_chunks(f, chunkids):
             raise Fail(filename, 'data changed')
 
@@ -166,13 +170,13 @@ class VerifyPlugin(obnamlib.ObnamPlugin):
 
     def verify_chunks(self, f, chunkids):
         for chunkid in chunkids:
-            backed_up = self.repo.get_chunk(chunkid)
+            backed_up = self.repo.get_chunk_content(chunkid)
             live_data = f.read(len(backed_up))
             if backed_up != live_data:
                 return False
         return True
 
-    def walk(self, gen, args):
+    def walk(self, gen_id, args):
         '''Iterate over each pathname specified by arguments.
 
         This is a generator.
@@ -182,6 +186,59 @@ class VerifyPlugin(obnamlib.ObnamPlugin):
         for arg in args:
             scheme, netloc, path, query, fragment = urlparse.urlsplit(arg)
             arg = os.path.normpath(path)
-            for x in self.repo.walk(gen, arg):
-                yield x
+            for x in self.repo_walk(gen_id, arg):
+                yield x, self.construct_metadata_object(gen_id, x)
 
+    def repo_walk(self, gen_id, dirname, depth_first=False):
+        # FIXME: this is duplicate code.
+        '''Like os.walk, but for a generation.
+
+        This is a generator. Each return value is a tuple consisting
+        of a pathname and its corresponding metadata. Directories are
+        recursed into.
+
+        '''
+
+        arg = os.path.normpath(dirname)
+        mode = self.repo.get_file_key(
+            gen_id, dirname, obnamlib.REPO_FILE_MODE)
+        if stat.S_ISDIR(mode):
+            if not depth_first:
+                yield dirname
+            kidpaths = self.repo.get_file_children(gen_id, dirname)
+            for kp in kidpaths:
+                for x in self.repo_walk(gen_id, kp, depth_first=depth_first):
+                    yield x
+            if depth_first:
+                yield arg
+        else:
+            yield arg
+
+    def construct_metadata_object(self, gen, filename):
+        # FIXME: this is duplicate code.
+        allowed = set(self.repo.get_allowed_file_keys())
+        def K(key):
+            if key in allowed:
+                return self.repo.get_file_key(gen, filename, key)
+            else:
+                return None
+
+        return obnamlib.Metadata(
+            st_atime_sec=K(obnamlib.REPO_FILE_ATIME_SEC),
+            st_atime_nsec=K(obnamlib.REPO_FILE_ATIME_NSEC),
+            st_mtime_sec=K(obnamlib.REPO_FILE_MTIME_SEC),
+            st_mtime_nsec=K(obnamlib.REPO_FILE_MTIME_NSEC),
+            st_blocks=K(obnamlib.REPO_FILE_BLOCKS),
+            st_dev=K(obnamlib.REPO_FILE_DEV),
+            st_gid=K(obnamlib.REPO_FILE_GID),
+            st_ino=K(obnamlib.REPO_FILE_INO),
+            st_mode=K(obnamlib.REPO_FILE_MODE),
+            st_nlink=K(obnamlib.REPO_FILE_NLINK),
+            st_size=K(obnamlib.REPO_FILE_SIZE),
+            st_uid=K(obnamlib.REPO_FILE_UID),
+            username=K(obnamlib.REPO_FILE_USERNAME),
+            groupname=K(obnamlib.REPO_FILE_GROUPNAME),
+            target=K(obnamlib.REPO_FILE_SYMLINK_TARGET),
+            xattr=K(obnamlib.REPO_FILE_XATTR_BLOB) or None,
+            md5=K(obnamlib.REPO_FILE_MD5),
+            )
