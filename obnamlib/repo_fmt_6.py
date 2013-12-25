38abe70b1b8872b7708fc1f0354f51bfc88caa79
Lars Wirzenius <liw@liw.fi>
2013-12-25 13:12:32 +0000

    Remove uninteresting FIXME

--- obnamlib/repo_fmt_6.py
+++ obnamlib/repo_fmt_6.py
@@ -387,10 +387,6 @@ class RepositoryFormat6(obnamlib.RepositoryInterface):
                 except KeyError:
                     # No checksum, therefore it can't be shared, therefore
                     # we can remove it.
-                    # FIXME: shouldn't this be: no checksum, so we DON'T
-                    # KNOW if it's safe to remove. Actually, there's
-                    # no guarantee a chunk can be removed, if we ever
-                    # allow a chunk to not be in the checksum trees. Hmm.
                     self.remove_chunk(chunk_id)
                 else:
                     self.remove_chunk_from_indexes(chunk_id, client_name)
