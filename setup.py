694fac554b55d68915ee64704d08698d09a6cfd3
Lars Wirzenius <liw@liw.fi>
2014-03-15 17:19:50 +0000

    Prepare release version 1.7

--- setup.py
+++ setup.py
@@ -194,7 +194,7 @@ class Check(Command):
 
 
 setup(name='obnam',
-      version='1.6.1',
+      version='1.7',
       description='Backup software',
       author='Lars Wirzenius',
       author_email='liw@liw.fi',
