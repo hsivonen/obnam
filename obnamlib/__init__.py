ed46cb4757256ba1797fced95a2bce07f868ba63
Lars Wirzenius <liw@liw.fi>
2014-03-22 09:12:28 +0000

    Prepare for release version 1.7.1

--- obnamlib/__init__.py
+++ obnamlib/__init__.py
@@ -17,7 +17,7 @@
 import cliapp
 
 
-__version__ = '1.7'
+__version__ = '1.7.1'
 
 
 
--- setup.py
+++ setup.py
@@ -194,7 +194,7 @@ class Check(Command):
 
 
 setup(name='obnam',
-      version='1.7',
+      version='1.7.1',
       description='Backup software',
       author='Lars Wirzenius',
       author_email='liw@liw.fi',
