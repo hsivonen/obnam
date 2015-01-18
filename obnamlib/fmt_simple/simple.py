1436c0d54605f23a6891cbc07f37455ef1481fd4
Lars Wirzenius <liw@liw.fi>
2015-01-18 20:55:00 +0200

    Mark stuff outside coverage

--- obnamlib/fmt_simple/simple.py
+++ obnamlib/fmt_simple/simple.py
@@ -748,7 +748,7 @@ class RepositoryFormatSimple(obnamlib.RepositoryInterface):
     def get_allowed_client_keys(self):
         return []
 
-    def get_client_key(self, client_name, key):
+    def get_client_key(self, client_name, key): # pragma: no cover
         raise obnamlib.RepositoryClientKeyNotAllowed(
             format=self.format,
             client_name=client_name,
@@ -760,7 +760,7 @@ class RepositoryFormatSimple(obnamlib.RepositoryInterface):
             client_name=client_name,
             key_name=obnamlib.repo_key_name(key))
 
-    def get_client_extra_data_directory(self, client_name):
+    def get_client_extra_data_directory(self, client_name): # pragma: no cover
         if client_name not in self.get_client_names():
             raise obnamlib.RepositoryClientDoesNotExist(
                 client_name=client_name)
