8dde89ecb7208b1c7e50c31e6b6de302eebc2b45
Lars Wirzenius <liw@liw.fi>
2015-01-18 19:38:10 +0200

    Implement get_file_chunk_ids

--- obnamlib/fmt_simple/simple.py
+++ obnamlib/fmt_simple/simple.py
@@ -380,6 +380,15 @@ class SimpleClient(SimpleToplevel):
 
         files[filename]['keys'][key_name] = value
 
+    def get_file_chunk_ids(self, gen_number, filename):
+        generation = self._lookup_generation_by_gen_number(gen_number)
+        if filename not in generation['files']:
+            raise obnamlib.RepositoryFileDoesNotExistInGeneration(
+                client_name=self._client_name,
+                genspec=gen_number,
+                filename=filename)
+        return generation['files'][filename]['chunks']
+
 
 class GenerationId(object):
 
@@ -715,7 +724,8 @@ class RepositoryFormatSimple(obnamlib.RepositoryInterface):
                 obnamlib.REPO_FILE_MD5]
 
     def get_file_chunk_ids(self, generation_id, filename):
-        raise NotImplementedError()
+        client = self._lookup_client_by_generation(generation_id)
+        return client.get_file_chunk_ids(generation_id.gen_number, filename)
 
     def append_file_chunk_id(self, generation_id, filename, chunk_id):
         raise NotImplementedError()
