de611205225f4c81f14a7c6ee27057624e8e9cfe
Lars Wirzenius <liw@liw.fi>
2015-01-24 22:00:56 +0200

    Fix copyright years, add missing gpl3 blurb

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# Copyright (C) 2008-2014  Lars Wirzenius <liw@liw.fi>
+# Copyright (C) 2008-2015  Lars Wirzenius <liw@liw.fi>
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
