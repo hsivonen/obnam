e8d1768ac9724ddad2c7f3f3bcec955071cbaed6
Lars Wirzenius <liw@liw.fi>
2015-05-03 23:09:48 +0300

    Use RepositoryDelegator in simple format

--- obnamlib/fmt_simple/simple.py
+++ obnamlib/fmt_simple/simple.py
@@ -515,42 +515,6 @@ class GenerationId(object):
         return 'GenerationId(%s,%s)' % (self.client_name, self.gen_number)
 
 
-class ClientFinder(object):
-
-    def __init__(self):
-        self._fs = None
-        self._lockmgr = None
-        self._client_list = None
-        self._clients = {}
-        self._current_time = None
-
-    def set_fs(self, fs):
-        self._fs = fs
-
-    def set_lock_manager(self, lockmgr):
-        self._lockmgr = lockmgr
-
-    def set_client_list(self, client_list):
-        self._client_list = client_list
-
-    def set_current_time(self, current_time):
-        self._current_time = current_time
-
-    def find_client(self, client_name):
-        if client_name not in self._client_list.get_client_names():
-            raise obnamlib.RepositoryClientDoesNotExist(
-                client_name=client_name)
-
-        if client_name not in self._clients:
-            client = SimpleClient(client_name)
-            client.set_fs(self._fs)
-            client.set_lock_manager(self._lockmgr)
-            client.set_current_time(self._current_time)
-            self._clients[client_name] = client
-
-        return self._clients[client_name]
-
-
 class SimpleChunkStore(object):
 
     def __init__(self):
@@ -711,7 +675,7 @@ class SimpleChunkIndexes(SimpleToplevel):
         return None
 
 
-class RepositoryFormatSimple(obnamlib.RepositoryInterface):
+class RepositoryFormatSimple(obnamlib.RepositoryDelegator):
 
     '''Simplistic repository format as an example.
 
@@ -722,36 +686,11 @@ class RepositoryFormatSimple(obnamlib.RepositoryInterface):
     format = 'simple'
 
     def __init__(self, **kwargs):
-        self._fs = None
-        self._hooks = kwargs['hooks']
-        self._lock_timeout = kwargs.get('lock_timeout', 0)
-
-        self._client_list = SimpleClientList()
-        self._chunk_store = SimpleChunkStore()
-        self._chunk_indexes = SimpleChunkIndexes()
-
-        self._client_finder = ClientFinder()
-        self._client_finder.set_client_list(self._client_list)
-        self._client_finder.set_current_time(kwargs['current_time'])
-
-    def get_fs(self):
-        return self._fs.fs
-
-    def set_fs(self, fs):
-        self._fs = obnamlib.RepositoryFS(self, fs, self._hooks)
-        self._lockmgr = obnamlib.LockManager(self._fs, self._lock_timeout, '')
-
-        self._client_list.set_fs(self._fs)
-        self._client_list.set_lock_manager(self._lockmgr)
-        self._client_list.set_hooks(self._hooks)
-
-        self._client_finder.set_fs(self._fs)
-        self._client_finder.set_lock_manager(self._lockmgr)
-
-        self._chunk_store.set_fs(self._fs)
-
-        self._chunk_indexes.set_fs(self._fs)
-        self._chunk_indexes.set_lock_manager(self._lockmgr)
+        obnamlib.RepositoryDelegator.__init__(self, **kwargs)
+        self.set_client_list_object(SimpleClientList())
+        self.set_chunk_store_object(SimpleChunkStore())
+        self.set_chunk_indexes_object(SimpleChunkIndexes())
+        self.set_client_factory(SimpleClient)
 
     def init_repo(self):
         pass
@@ -766,67 +705,8 @@ class RepositoryFormatSimple(obnamlib.RepositoryInterface):
         return ['client-list', 'chunk-store', 'chunk-indexes']
 
     #
-    # Client list methods.
-    #
-
-    def get_client_names(self):
-        return self._client_list.get_client_names()
-
-    def lock_client_list(self):
-        self._client_list.lock()
-
-    def unlock_client_list(self):
-        self._client_list.unlock()
-
-    def commit_client_list(self):
-        self._client_list.commit()
-
-    def got_client_list_lock(self):
-        return self._client_list.got_lock
-
-    def force_client_list_lock(self):
-        return self._client_list.force_lock()
-
-    def add_client(self, client_name):
-        self._client_list.add_client(client_name)
-
-    def remove_client(self, client_name):
-        self._client_list.remove_client(client_name)
-
-    def rename_client(self, old_client_name, new_client_name):
-        self._client_list.rename_client(old_client_name, new_client_name)
-
-    def get_client_encryption_key_id(self, client_name):
-        return self._client_list.get_client_encryption_key_id(client_name)
-
-    def set_client_encryption_key_id(self, client_name, key_id):
-        return self._client_list.set_client_encryption_key_id(
-            client_name, key_id)
-
-    #
     # Per-client methods.
     #
-
-    def client_is_locked(self, client_name):
-        return self._lookup_client(client_name).is_locked()
-
-    def _lookup_client(self, client_name):
-        return self._client_finder.find_client(client_name)
-
-    def lock_client(self, client_name):
-        self._lookup_client(client_name).lock()
-
-    def unlock_client(self, client_name):
-        self._lookup_client(client_name).unlock()
-
-    def commit_client(self, client_name):
-        self._lookup_client(client_name).commit()
-
-    def got_client_lock(self, client_name):
-        return self._lookup_client(client_name).got_lock
-
-    def force_client_lock(self, client_name):
-        self._lookup_client(client_name).force_lock()
 
     def get_allowed_client_keys(self):
         return []
@@ -849,12 +729,6 @@ class RepositoryFormatSimple(obnamlib.RepositoryInterface):
                 client_name=client_name)
         return client_name
 
-    def get_client_generation_ids(self, client_name):
-        return self._lookup_client(client_name).get_client_generation_ids()
-
-    def create_generation(self, client_name):
-        return self._lookup_client(client_name).create_generation()
-
     def get_allowed_generation_keys(self):
         return [
             obnamlib.REPO_GENERATION_TEST_KEY,
@@ -864,80 +738,6 @@ class RepositoryFormatSimple(obnamlib.RepositoryInterface):
             obnamlib.REPO_GENERATION_FILE_COUNT,
             obnamlib.REPO_GENERATION_TOTAL_DATA,
             ]
-
-    def get_generation_key(self, generation_id, key):
-        client = self._lookup_client_by_generation(generation_id)
-        return client.get_generation_key(generation_id.gen_number, key)
-
-    def _lookup_client_by_generation(self, generation_id):
-        return self._lookup_client(generation_id.client_name)
-
-    def set_generation_key(self, generation_id, key, value):
-        if key not in self.get_allowed_generation_keys():
-            raise obnamlib.RepositoryGenerationKeyNotAllowed(
-                client_name=generation_id.client_name,
-                format=self.format,
-                key_name=obnamlib.repo_key_name(key))
-        client = self._lookup_client_by_generation(generation_id)
-        return client.set_generation_key(generation_id.gen_number, key, value)
-
-    def remove_generation(self, generation_id):
-        client = self._lookup_client_by_generation(generation_id)
-        return client.remove_generation(generation_id.gen_number)
-
-    def get_generation_chunk_ids(self, generation_id):
-        client = self._lookup_client_by_generation(generation_id)
-        return client.get_generation_chunk_ids(generation_id.gen_number)
-
-    def interpret_generation_spec(self, client_name, genspec):
-        ids = self.get_client_generation_ids(client_name)
-        if not ids:
-            raise obnamlib.RepositoryClientHasNoGenerations(
-                client_name=client_name)
-
-        if genspec == 'latest':
-            return ids[-1]
-
-        for gen_id in ids:
-            if self.make_generation_spec(gen_id) == genspec:
-                return gen_id
-
-        raise obnamlib.RepositoryGenerationDoesNotExist(
-            client_name=client_name, gen_id=genspec)
-
-    def make_generation_spec(self, generation_id):
-        return generation_id.gen_number
-
-    def file_exists(self, generation_id, filename):
-        client = self._lookup_client_by_generation(generation_id)
-        return client.file_exists(generation_id.gen_number, filename)
-
-    def add_file(self, generation_id, filename):
-        client = self._lookup_client_by_generation(generation_id)
-        return client.add_file(generation_id.gen_number, filename)
-
-    def remove_file(self, generation_id, filename):
-        client = self._lookup_client_by_generation(generation_id)
-        return client.remove_file(generation_id.gen_number, filename)
-
-    def get_file_key(self, generation_id, filename, key):
-        if key not in self.get_allowed_file_keys():
-            raise obnamlib.RepositoryFileKeyNotAllowed(
-                client_name=generation_id.client_name,
-                format=self.format)
-
-        client = self._lookup_client_by_generation(generation_id)
-        return client.get_file_key(generation_id.gen_number, filename, key)
-
-    def set_file_key(self, generation_id, filename, key, value):
-        if key not in self.get_allowed_file_keys():
-            raise obnamlib.RepositoryFileKeyNotAllowed(
-                client_name=generation_id.client_name,
-                format=self.format)
-
-        client = self._lookup_client_by_generation(generation_id)
-        return client.set_file_key(
-            generation_id.gen_number, filename, key, value)
 
     def get_allowed_file_keys(self):
         return [obnamlib.REPO_FILE_TEST_KEY,
@@ -959,76 +759,21 @@ class RepositoryFormatSimple(obnamlib.RepositoryInterface):
                 obnamlib.REPO_FILE_INO,
                 obnamlib.REPO_FILE_MD5]
 
-    def get_file_chunk_ids(self, generation_id, filename):
-        client = self._lookup_client_by_generation(generation_id)
-        return client.get_file_chunk_ids(generation_id.gen_number, filename)
+    def interpret_generation_spec(self, client_name, genspec):
+        ids = self.get_client_generation_ids(client_name)
+        if not ids:
+            raise obnamlib.RepositoryClientHasNoGenerations(
+                client_name=client_name)
 
-    def append_file_chunk_id(self, generation_id, filename, chunk_id):
-        client = self._lookup_client_by_generation(generation_id)
-        return client.append_file_chunk_id(
-            generation_id.gen_number, filename, chunk_id)
+        if genspec == 'latest':
+            return ids[-1]
 
-    def clear_file_chunk_ids(self, generation_id, filename):
-        client = self._lookup_client_by_generation(generation_id)
-        return client.clear_file_chunk_ids(generation_id.gen_number, filename)
+        for gen_id in ids:
+            if self.make_generation_spec(gen_id) == genspec:
+                return gen_id
 
-    def get_file_children(self, generation_id, filename):
-        client = self._lookup_client_by_generation(generation_id)
-        return client.get_file_children(generation_id.gen_number, filename)
+        raise obnamlib.RepositoryGenerationDoesNotExist(
+            client_name=client_name, gen_id=genspec)
 
-    #
-    # Chunk storage methods.
-    #
-
-    def put_chunk_content(self, content):
-        return self._chunk_store.put_chunk_content(content)
-
-    def get_chunk_content(self, chunk_id):
-        return self._chunk_store.get_chunk_content(chunk_id)
-
-    def has_chunk(self, chunk_id):
-        return self._chunk_store.has_chunk(chunk_id)
-
-    def remove_chunk(self, chunk_id):
-        return self._chunk_store.remove_chunk(chunk_id)
-
-    def get_chunk_ids(self):
-        return self._chunk_store.get_chunk_ids()
-
-    #
-    # Chunk indexes methods.
-    #
-
-    def lock_chunk_indexes(self):
-        self._chunk_indexes.lock()
-
-    def unlock_chunk_indexes(self):
-        self._chunk_indexes.unlock()
-
-    def commit_chunk_indexes(self):
-        self._chunk_indexes.commit()
-
-    def got_chunk_indexes_lock(self):
-        return self._chunk_indexes.got_lock
-
-    def force_chunk_indexes_lock(self):
-        self._chunk_indexes.force_lock()
-
-    def prepare_chunk_for_indexes(self, chunk_content):
-        return self._chunk_indexes.prepare_chunk_for_indexes(chunk_content)
-
-    def put_chunk_into_indexes(self, chunk_id, token, client_id):
-        return self._chunk_indexes.put_chunk_into_indexes(
-            chunk_id, token, client_id)
-
-    def find_chunk_ids_by_content(self, chunk_content):
-        return self._chunk_indexes.find_chunk_ids_by_content(chunk_content)
-
-    def remove_chunk_from_indexes(self, chunk_id, client_id):
-        self._chunk_indexes.remove_chunk_from_indexes(chunk_id, client_id)
-
-    def remove_chunk_from_indexes_for_all_clients(self, chunk_id):
-        self._chunk_indexes.remove_chunk_from_indexes_for_all_clients(chunk_id)
-
-    def validate_chunk_content(self, chunk_id):
-        return self._chunk_indexes.validate_chunk_content(chunk_id)
+    def make_generation_spec(self, generation_id):
+        return generation_id.gen_number
