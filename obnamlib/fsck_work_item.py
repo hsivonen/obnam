f61773add4c34fd3e9186eee5ca55f729ee11bb1
Lars Wirzenius <liw@liw.fi>
2015-08-29 11:02:44 +0300

    Update copyright year

--- obnamlib/fsck_work_item.py
+++ obnamlib/fsck_work_item.py
@@ -1,4 +1,4 @@
-# Copyright 2013  Lars Wirzenius
+# Copyright 2013,2015  Lars Wirzenius
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
