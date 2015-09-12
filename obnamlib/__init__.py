1f2a0180c6f7fd7f0d6fbf6b977fe67c5020af4a
Lars Wirzenius <liw@liw.fi>
2015-09-12 12:40:40 +0300

    Prepare release version 1.17

--- obnamlib/__init__.py
+++ obnamlib/__init__.py
@@ -17,7 +17,7 @@
 import cliapp
 
 
-__version__ = '1.16'
+__version__ = '1.17'
 
 
 # Import _obnam if it is there. We need to be able to do things without
--- setup.py
+++ setup.py
@@ -301,7 +301,7 @@ class Check(Command):
 
 
 setup(name='obnam',
-      version='1.16',
+      version='1.17',
       description='Backup software',
       author='Lars Wirzenius',
       author_email='liw@liw.fi',
